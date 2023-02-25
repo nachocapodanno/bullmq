328379ec1f9367cfc91b0ce622d86de46524d8d9
Ignacio Capodanno <ignacio.capodanno@cookunity.com>
2023-02-25 01:21:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import { Queue, Worker } from 'bullmq';
+import IORedis from 'ioredis';
+
+// Setup
+const QUEUE_NAME = 'pokedex';
+const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+const getPokemonOrderByName = (pokemonName) =>
+  fetch(`${POKEMON_URL}${pokemonName}`)
+    .then((res) => res.json())
+    .then(({ order }) => order);
+
+// Queue & Worker
+const connection = new IORedis();
+const todoQueue = new Queue(QUEUE_NAME, { connection });
+const worker = new Worker(
+  QUEUE_NAME,
+  async (job) => {
+    const { name } = job.data;
+    const order = await getPokemonOrderByName(name);
+    console.log(`${name} is in position no. ${order}.`);
+  },
+  { connection, concurrency: 10 }
+);
+
+// Jobs
+await todoQueue.add('select pokemon', { name: 'pikachu' });
+await todoQueue.add('select pokemon', { name: 'charmander' });
+await todoQueue.add('select pokemon', { name: 'bulbasaur' });
+await todoQueue.add('select pokemon', { name: 'squirtle' });
+
+// Worker events
+worker.on('completed', (job) => {
+  console.log(`${job.id} has completed!`);
+});
+
+worker.on('failed', (job, err) => {
+  console.log(`${job.id} has failed with ${err.message}`);
+});
